8bcd3eb051a010a4bce89e02db5846c9cb1b3b05
megoo <muyidixin2006@126.com>
2018-07-25 19:07:40 +0800

    修改文件头

--- SM4/init/run_sm4.c
+++ SM4/init/run_sm4.c
@@ -7,44 +7,39 @@
 
 #include <string.h>
 #include <stdio.h>
+
 #include "sm4.h"
 
-int main(int argc, char** argv)
-{
-	unsigned int mode;
-	unsigned char key[16];
-	unsigned char input[16];
-	
-	unsigned char output[16];
-	sm4_context ctx;
-	unsigned long i;
-	
-	//SM4 input 读取
-	format_park_input(argv[1], input);
-	
-	//SM4 key 读取
-	format_park_input(argv[2], key);
-	
-	// SM4 mode:encrypt=1/decrypt=0
-	mode = atoi(argv[3]);
-	
-	if(mode == SM4_ENCRYPT)
-	{
-		//encrypt standard testing vector
-		sm4_setkey_enc(&ctx, key);
-		sm4_crypt_ecb(&ctx, 1, 16, input, output);
-	}
-	else
-	{
-		//decrypt standard testing vector
-		sm4_setkey_dec(&ctx, key);
-		sm4_crypt_ecb(&ctx, 0, 16, output, output);
-	}
-	
-	//SM4 输出
-	for (i = 0; i < 16; i++)
-		printf("%02x ", output[i]);
-	printf("\n");
+int main(int argc, char **argv) {
+    int mode = 0, flag = 1;
+    unsigned char key[16];
+    unsigned char iv[16] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+                            0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
+    unsigned char input[16];
 
-	return 0;
+    unsigned char output[16];
+    sm4_context ctx;
+    long i;
+
+    //SM4 input 读取
+    format_park_input(argv[1], input);
+
+    //SM4 key 读取
+    format_park_input(argv[2], key);
+
+    // SM4 mode:SM4_ECB 0,SM4_CBC 1,SM4_CFB 2,SM4_OFB 3,SM4_CTR 4
+    mode = atoi(argv[3]);
+    printf("mode:%d\n", mode);
+    // SM4 flag:encrypt=1/decrypt=0
+    flag = atoi(argv[4]);
+    printf("flag:%d\n", flag);
+
+    sm4(key, mode, flag, 16, iv, input, output);
+
+    //SM4 输出
+    for (i = 0; i < 16; i++)
+        printf("%02x ", output[i]);
+    printf("\n");
+
+    return 0;
 }
--- SM4/substep/sm4.h
+++ SM4/substep/sm4.h
@@ -4,21 +4,32 @@
        > E-mail:muyidixin2006@126.com
        > Created Time: Thu July 13 23:55:50 2018
 ************************************************************************/
+
 #ifndef XYSSL_SM4_H
 #define XYSSL_SM4_H
 
+/***
+ *sm4 select encrypt/decrypt by flag
+ ***/
 #define SM4_ENCRYPT     1
 #define SM4_DECRYPT     0
+
+/***
+ *sm4 select mode by mode
+ ***/
+#define SM4_ECB 0
+#define SM4_CBC 1
+#define SM4_CFB 2
+#define SM4_OFB 3
+#define SM4_CTR 4
 
 /**
  * \brief          SM4 context structure
  */
-typedef struct
-{
-	int mode;                   /*!<  encrypt/decrypt   */
-	unsigned long sk[32];       /*!<  SM4 subkeys       */
-}
-sm4_context;
+typedef struct {
+    int mode;                   /*!<  encrypt/decrypt   */
+    unsigned long sk[32];       /*!<  SM4 subkeys       */
+}sm4_context;
 
 
 #ifdef __cplusplus
@@ -31,7 +42,7 @@ extern "C" {
  * \param ctx      SM4 context to be initialized
  * \param key      16-byte secret key
  */
-void sm4_setkey_enc( sm4_context *ctx, unsigned char key[16] );
+void sm4_setkey_enc(sm4_context *ctx, unsigned char key[16]);
 
 /**
  * \brief          SM4 key schedule (128-bit, decryption)
@@ -39,37 +50,112 @@ void sm4_setkey_enc( sm4_context *ctx, unsigned char key[16] );
  * \param ctx      SM4 context to be initialized
  * \param key      16-byte secret key
  */
-void sm4_setkey_dec( sm4_context *ctx, unsigned char key[16] );
+void sm4_setkey_dec(sm4_context *ctx, unsigned char key[16]);
 
 /**
  * \brief          SM4-ECB block encryption/decryption
  * \param ctx      SM4 context
- * \param mode     SM4_ENCRYPT or SM4_DECRYPT
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
  * \param length   length of the input data
  * \param input    input block
  * \param output   output block
  */
-void sm4_crypt_ecb( sm4_context *ctx,
-                    int mode,
-                    int length,
-                    unsigned char *input,
-                    unsigned char *output);
+void sm4_crypt_ecb(sm4_context *ctx,
+                   int flag,
+                   int length,
+                   unsigned char *input,
+                   unsigned char *output);
 
 /**
  * \brief          SM4-CBC buffer encryption/decryption
  * \param ctx      SM4 context
- * \param mode     SM4_ENCRYPT or SM4_DECRYPT
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
  * \param length   length of the input data
  * \param iv       initialization vector (updated after use)
  * \param input    buffer holding the input data
  * \param output   buffer holding the output data
  */
-void sm4_crypt_cbc( sm4_context *ctx,
-                    int mode,
-                    int length,
-                    unsigned char iv[16],
-                    unsigned char *input,
-                    unsigned char *output );
+void sm4_crypt_cbc(sm4_context *ctx,
+                   int flag,
+                   int length,
+                   unsigned char iv[16],
+                   unsigned char *input,
+                   unsigned char *output);
+
+/**
+ * \brief          SM4-CFB encryption/decryption
+ * \param ctx      SM4 context
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
+ * \param length   length of the input data
+ * \param iv       initialization vector (updated after use)
+ * \param input    buffer holding the input data
+ * \param output   buffer holding the output data
+ */
+void sm4_crypt_cfb(sm4_context *ctx,
+                   int flag,
+                   int length,
+                   unsigned char iv[16],
+                   unsigned char *input,
+                   unsigned char *output);
+
+
+/**
+ * \brief          SM4-OFB encryption/decryption
+ * \param ctx      SM4 context
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
+ * \param length   length of the input data
+ * \param iv       initialization vector (updated after use)
+ * \param input    buffer holding the input data
+ * \param output   buffer holding the output data
+ */
+void sm4_crypt_ofb(sm4_context *ctx,
+                   int flag,
+                   int length,
+                   unsigned char iv[16],
+                   unsigned char *input,
+                   unsigned char *output);
+
+
+/**
+ * \brief          SM4-CTR encryption/decryption
+ * \param ctx      SM4 context
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
+ * \param length   length of the input data
+ * \param iv       initialization vector (updated after use)
+ * \param input    buffer holding the input data
+ * \param output   buffer holding the output data
+ */
+void sm4_crypt_ctr(sm4_context *ctx,
+                   int flag,
+                   int length,
+                   unsigned char iv[16],
+                   unsigned char *input,
+                   unsigned char *output);
+
+/**
+ * \brief          SM4 encryption/decryption
+ * \param key      SM4 key
+ * \param flag     SM4_ENCRYPT or SM4_DECRYPT
+ * \param length   length of the input data
+ * \param iv       initialization vector (updated after use)
+ * \param input    buffer holding the input data
+ * \param output   buffer holding the output data
+ */
+void sm4(unsigned char key[16],
+         int mode,
+         int flag,
+         int length,
+         unsigned char iv[16],
+         unsigned char *input,
+         unsigned char *output);
+
+/**
+ * \brief          SM4 parameter format hex
+ * \param input    buffer holding the input data
+ * \param output   buffer holding the output data
+ */
+void format_park_input(unsigned char *input,
+                       unsigned char *output);
 
 #ifdef __cplusplus
 }
